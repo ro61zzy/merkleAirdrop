9962a5209de39cde6543e22927e09aa19577679f
rozzy <rosekaremeri@gmail.com>
2024-09-02 22:34:23 +0300

    create claim function

--- contracts/MerkleAirdrop.sol
+++ contracts/MerkleAirdrop.sol
@@ -9,9 +9,26 @@ contract MerkleAirdrop is Ownable {
     IERC20 public token;
     bytes32 public merkleRoot;
 
+     mapping(address => bool) public hasClaimed;
+
       constructor(IERC20 _token, bytes32 _merkleRoot) {
         token = _token;
         merkleRoot = _merkleRoot;
+    }
+
+function claim(uint256 amount, bytes32[] calldata merkleProof) external {
+        require(!hasClaimed[msg.sender], "Airdrop already claimed");
+
+        // Verify the Merkle proof
+        bytes32 node = keccak256(abi.encodePacked(msg.sender, amount));
+        require(MerkleProof.verify(merkleProof, merkleRoot, node), "Invalid proof");
+
+        // Mark the address as having claimed
+        hasClaimed[msg.sender] = true;
+
+        // Transfer the tokens
+        require(token.transfer(msg.sender, amount), "Transfer failed");
+
     }
 
 
